008f542be5caffad7d07f5734215886480fc4647
Adrian Schurz <schurz.adrian@gmail.com>
2020-06-29 13:34:56 +0200

    add more led boxes

--- dataExtraction.py
+++ dataExtraction.py
@@ -44,11 +44,65 @@ cropBox = (leftmost, topmost, rightmost, bottom)
 nonZeroRegion = redMask.crop(cropBox)
 
 # draw on the image
-def drawRectangle(img, x0, y0, x1, y1):
-    draw = ImageDraw.Draw(img)
-    draw.rectangle([x0,y0,x1,y1], outline='red')
+
+class Rectangle:
+    label = ''
+    lengthOfLongSide = 85
+    lengthOfShortSide = 10
+    
+    originX, originY = 0, 0
+    lengthX, lengthY = 85, 10
+    orientation = 'horizontal'
+    color = 'red'
+    
+    def __init__(self, originX, originY, orientation='horizontal', label = ''):
+        self.orientation = orientation
+        self.originX = originX
+        self.originY = originY
+        self.label = label
+        self.sideLengthsFromOrientation()
+        
+    def sideLengthsFromOrientation(self):
+        if self.orientation == 'horizontal':
+            self.lengthX = self.lengthOfLongSide
+            self.lengthY = self.lengthOfShortSide
+        else:
+            self.lengthX = self.lengthOfShortSide
+            self.lengthY = self.lengthOfLongSide
+    
+    def drawOn(self, image):
+        drawable = ImageDraw.Draw(image)        
+        oppositeCornerX = self.originX + self.lengthX
+        oppositeCornerY = self.originY + self.lengthY
+        rectangleCoordinates = [
+            self.originX,
+            self.originY,
+            oppositeCornerX,
+            oppositeCornerY
+        ]
+        
+        drawable.rectangle(rectangleCoordinates, outline=self.color)
+        drawable.text((self.originX + 2, self.originY), self.label, fill='green')
     
 regionAsRGB = nonZeroRegion.convert('RGBA')
-drawRectangle(regionAsRGB, 40, 5, 125, 15)
+
+horizontal = 'horizontal'
+vertical = 'vertical'
+
+rectangleCoordinates = [
+        ('1', 40, 5, horizontal),
+        ('2', 8, 37, vertical),
+        ('3', 157, 34, vertical),
+        ('4', 40, 135, horizontal),
+        ('5', 18, 158, vertical),
+        ('6', 163, 158, vertical),
+        ('7', 60, 248, horizontal),
+        ('10', 397, 28, vertical),
+        ('13', 387, 157, vertical)
+]
+
+for coord in rectangleCoordinates:
+    rectangle = Rectangle(coord[1], coord[2], coord[3], label=coord[0])
+    rectangle.drawOn(regionAsRGB)
 
 regionAsRGB.show()
