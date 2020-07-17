d4b4a268dbb7e9b9693c13866f6670c26981d531
Adrian Schurz <schurz.adrian@gmail.com>
2020-07-17 22:04:56 +0200

    refactoring, recalibrate after moved camera, send to db

--- dataExtraction.py
+++ dataExtraction.py
@@ -1,83 +1,94 @@
 from PIL import Image, ImageChops
 import subprocess
 import os
-Image.MAX_IMAGE_PIXELS = None
 
-# load image
-image = Image.open('current.jpg')
+class ScaleReader:
+    weight = 0
+    inputImage = None
+    transformedImage = None
+    redMaskImage = None
+    filteredFilename = 'current_filtered.jpg'
+    
+    def loadImage(self):
+        self.inputImage = Image.open('current.jpg')
+        
+    def readDigits(self):
+        # compensate for rotational offset between camera and scale, rotates counterclockwise
+        rotationAngle = 201
+        rotated = self.inputImage.rotate(rotationAngle)
 
-# compensate for rotational offset between camera and scale
-rotationAngle = 191
-rotated = image.rotate(rotationAngle)
+        # approximately crop scale display
+        cropBox = (335, 245, 690, 420)
+        cropped = rotated.crop(cropBox)
 
-# TODO update crop and remove translation
-# compensate for translational offset between camera and scale
-offsetX, offsetY = -100, 400
-translated = ImageChops.offset(rotated, offsetX, offsetY)
+        # transform to try and restore horizontal and vertical lines
+        nw = (0,0)
+        sw = (20,170)
+        se = (330, 180)
+        ne = (370,0)
+        transformed = cropped.transform(cropped.size, Image.QUAD,
+                                        [
+                                            nw[0],nw[1],sw[0],sw[1],se[0],se[1],ne[0],ne[1]
+                                        ],
+                                        Image.BILINEAR)
+        self.transformedImage = transformed
 
-# approximately crop scale display
-cropBox = (330, 000, 680, 180)
-cropped = translated.crop(cropBox)
+        # try to only retain red pixels
+        colorChannels = transformed.split()
+        redChannel = colorChannels[0]
 
-# transform to try and restore horizontal and vertical lines
-nw = (0,0)
-sw = (20,170)
-se = (330, 180)
-ne = (370,0)
-transformed = cropped.transform(cropped.size, Image.QUAD,
-                                [
-                                    nw[0],nw[1],sw[0],sw[1],se[0],se[1],ne[0],ne[1]
-                                ],
-                                Image.BILINEAR)
+        redMask = Image.new('L',(transformed.size[0], transformed.size[1]))
 
-# try to only retain red pixels
-colorChannels = transformed.split()
-redChannel = colorChannels[0]
+        for ix in range (transformed.size[0]):
+            for iy in range (transformed.size[1]):
+                pixel = transformed.getpixel((ix, iy))
+                red = pixel[0]
+                green = pixel[1]
+                blue = pixel[2]
+                totalIntensity = red + green + blue
+                if totalIntensity == 0:
+                    redMask.putpixel((ix, iy), 0)
+                else:
+                    redProportion = max(0, (red - green) - blue)
+                    if redProportion > 10:
+                        redMask.putpixel((ix, iy), 255)
+                    else:
+                        redMask.putpixel((ix, iy), 0)
 
-redMask = Image.new('L',(transformed.size[0], transformed.size[1]))
+        redMask.save(self.filteredFilename)
+        self.redMaskImage = redMask
+        currentDirectory = os.getcwd()
+        filePath = currentDirectory + '/' +  self.filteredFilename
 
-for ix in range (transformed.size[0]):
-    for iy in range (transformed.size[1]):
-        pixel = transformed.getpixel((ix, iy))
-        red = pixel[0]
-        green = pixel[1]
-        blue = pixel[2]
-        totalIntensity = red + green + blue
-        if totalIntensity == 0:
-            redMask.putpixel((ix, iy), 0)
-        else:
-            redProportion = max(0, (red - green) - blue)
-            if redProportion > 10:
-                redMask.putpixel((ix, iy), 255)
-            else:
-                redMask.putpixel((ix, iy), 0)
+        completed = subprocess.run(["ssocr", "invert", "-D", "-T", "-C", "-d", "-1", "-c", "digit", "-t", "25", filePath], stdout=subprocess.PIPE)
+        readout = completed.stdout
+        print(readout)
 
-filteredFilename = 'current_filtered.jpg'
-redMask.save(filteredFilename)
-currentDirectory = os.getcwd()
-filePath = currentDirectory + '/' +  filteredFilename
+        report = ''.join(list(filter(lambda x: x != '.', str(readout))))
 
-completed = subprocess.run(["ssocr", "invert", "-D", "-T", "-C", "-d", "-1", "-c", "digit", "-t", "25", filePath], stdout=subprocess.PIPE)
-readout = completed.stdout
-print(readout)
+        self.weight = report[2:4] + '.' + report[4]
 
-report = ''.join(list(filter(lambda x: x != '.', str(readout))))
+        print(self.weight)
+        print(completed.stderr)
+    
+    def textToSpeechTheValue(self):
+        text = f"The weight readout is is {self.weight}."
+        subprocess.run(["spd-say", text])
+        
+    def showDebugImages(self):
+        debugImage = Image.open('testbild.png')
+        debugImages = Image.new(mode='RGBA', size=(350, 180*3), color='grey')
+        debugImages.paste(self.transformedImage,(0,0))
+        debugImages.paste(self.redMaskImage,(0, 180))
+        debugImages.paste(debugImage,(0,360))
+        debugImages.show()
+        debugImages.save('debugImages.png')
+        os.remove('testbild.png')
+        os.remove(self.filteredFilename)
+        
+    def getWeight(self):
+        return self.weight
 
-report = report[2:4] + '.' + report[4]
 
-print(report)
-print(completed.stderr)
 
-subprocess.run(["spd-say", "weight readout"])
-subprocess.run(["spd-say", report])
 
-debugImage = Image.open('testbild.png')
-
-debugImages = Image.new(mode='RGBA', size=(350, 180*3), color='grey')
-debugImages.paste(transformed,(0,0))
-debugImages.paste(redMask,(0, 180))
-debugImages.paste(debugImage,(0,360))
-debugImages.show()
-debugImages.save('debugImages.png')
-os.remove('testbild.png')
-os.remove(filteredFilename)
--- imageAquisition.py
+++ imageAquisition.py
@@ -1,14 +1,65 @@
 from picamera import PiCamera
+import RPi.GPIO as GPIO
 from time import sleep
+from PIL import Image
+import requests
 import os
+from dataExtraction import ScaleReader
+
 
 currentDirectory = os.getcwd()
 filename = '/current.jpg'
 
-camera = PiCamera()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-camera.start_preview()
-sleep(8)
-camera.stop_preview()
+weight = 0
+        
+def takeAPicture():
+    camera = PiCamera()
+    camera.capture(currentDirectory + filename, resize=(1024, 576))
+    camera.close()
+    
+def showThePicture():
+    image = Image.open('current.jpg')
+    image.show()
 
-camera.capture(currentDirectory + filename, resize=(1024, 576))
+def takeAndShowPicture():
+    takeAPicture()
+    showThePicture()
+    
+def takePictureReadDigitsSpellThemOut():
+    sleep(7)
+    
+    takeAPicture()
+    
+    scaleReader = ScaleReader()
+    scaleReader.loadImage()
+    scaleReader.readDigits()
+    scaleReader.textToSpeechTheValue()
+    weight = scaleReader.getWeight()
+    
+    requestUrl = f"http://localhost:8086/write?db=sensors"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.post(requestUrl, data = f"telemetry weight={weight}", headers = headers)
+    print(response)
+    #scaleReader.showDebugImages()
+    
+def pinHighForAnotherWhile():
+    state = GPIO.input(10)
+    for sample in range(5):
+        state = state and GPIO.input(10)
+        sleep(0.001)
+    return state
+    
+def waitForPinHighStateThenDo(action):
+    print("waiting for button...")
+    while True:
+        if GPIO.input(10):
+            if pinHighForAnotherWhile():
+                print("button was pressed")
+                action()
+        sleep(0.1)
+
+waitForPinHighStateThenDo(takePictureReadDigitsSpellThemOut)
+
