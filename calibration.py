914df425ed1dc9f42a0d46161ee3ca48a1e3cec3
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-11-15 18:09:47 +0100

    Mouse clicks select coordinates. Key exports to config

--- calibration.py
+++ calibration.py
@@ -2,6 +2,7 @@ from picamera import PiCamera
 from PIL import Image, ImageTk
 import tkinter as tk
 import io
+import json
 
 camera = PiCamera()
 window = tk.Tk()
@@ -24,15 +25,33 @@ imagePosition = (width / 2, height / 2)
 canvas.create_image(imagePosition, image = image)
 canvas.pack(fill = tk.BOTH, expand = True)
 
+class Corners:
+    cornerIndex = 0
+    corners = [(),(),(),()]
+    def readCorner(self, event):
+        point = (event.x, event.y)
+        self.corners[self.cornerIndex] = point
+        self.cornerIndex += 1
+        self.cornerIndex = self.cornerIndex % 4
+        print(self.corners)
+    def getConfig(self):
+        config = {}
+        config['north-west'] = self.corners[0]
+        config['south-west'] = self.corners[1]
+        config['south-east'] = self.corners[2]
+        config['north-east'] = self.corners[3]
+        return config
+
+corners = Corners()
+
+def exportConfig(event):
+    print("exporting")
+    with open('config.json', 'w') as file:
+        json.dump(corners.getConfig(), file, indent = 4)
+        print("wrote config")
+    window.quit()
+
+canvas.bind('<Button-1>', corners.readCorner)
+window.bind('<Return>', exportConfig)
+
 window.mainloop()
-
-import json
-corners = {}
-corners['north-west'] = (0,0)
-corners['south-west'] = (20,170)
-corners['south-east'] = (33,180)
-corners['north-east'] = (370,0)
-
-with open('config.json', 'w') as file:
-    json.dump(corners, file, indent = 4)
-
