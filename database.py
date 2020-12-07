b78ba5e52c7cd409849b13bb06d564146610e9a3
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-12-07 18:18:36 +0100

    Code formatting

--- database.py
+++ database.py
@@ -1,13 +1,13 @@
 import subprocess
 
+
 class Database():
-    
     @staticmethod
     def writeWeight(weight):
         influxLine = f"INSERT telemetry weight={weight}"
-        
+
         subProcessOutput = subprocess.run(
             ['influx', '-database', 'sensors', '-execute', influxLine],
             stdout=subprocess.PIPE)
-        
+
         return subProcessOutput.stderr
