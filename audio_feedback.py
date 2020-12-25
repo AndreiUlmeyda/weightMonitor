8d0974d038fdc5618aeafd9219b6927e07f1e041
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-12-25 09:52:55 +0100

    Formatting

--- audio_feedback.py
+++ audio_feedback.py
@@ -2,6 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import play
 from time import sleep
 
+
 class AudioFeedback():
     def __init__(self):
         try:
@@ -10,10 +11,12 @@ class AudioFeedback():
             self.sound_success = AudioSegment.from_mp3("success.mp3")
             self.sound_error = AudioSegment.from_mp3("error.mp3") - 10
         except FileNotFoundError:
-            print('Error: This program provides audio feedback using the files: \n\
+            print(
+                'Error: This program provides audio feedback using the files: \n\
 start.mp3 \nin_progress.mp3 \nsuccess.mp3 \nerror.mp3 \n\
 One or more of those appear to be missing.\n\
-Please place respective files of appropriate content in your program directory.')
+Please place respective files of appropriate content in your program directory.'
+            )
 
     def start(self):
         play(self.sound_start)
