053ac524c4dbfa3e430b7f8d3be397a3c7e023fb
Adrian Schurz <ulmeyda.smile@gmail.com>
2020-12-31 17:46:11 +0100

    Test only writing plausible readouts

--- test_weight_monitor.py
+++ test_weight_monitor.py
@@ -8,18 +8,35 @@ from scale_reader import ScaleReader
 class TestWeightMonitor(unittest.TestCase):
     @patch('scale_reader.ScaleReader')
     def setUp(self, ScaleReaderMock) -> None:
-        audio_feedback = MagicMock()
-        ocr = ScaleReaderMock()
-        ocr.readWeight.return_value = 3
-        database = MagicMock()
-        raspberry = MagicMock()
-        config_loader = MagicMock()  # ConfigLoader(json)
-        self.monitor = WeightMonitor(audio_feedback=audio_feedback,
-                                     scale_reader=ocr,
-                                     database=database,
-                                     raspberry=raspberry,
-                                     config_loader=config_loader,
+        self.audio_feedback = MagicMock()
+        self.ocr = ScaleReaderMock()
+        self.database = MagicMock()
+        self.raspberry = MagicMock()
+        self.config_loader = MagicMock()
+        self.buildMonitor()
+
+    def buildMonitor(self):
+        self.monitor = WeightMonitor(audio_feedback=self.audio_feedback,
+                                     scale_reader=self.ocr,
+                                     database=self.database,
+                                     raspberry=self.raspberry,
+                                     config_loader=self.config_loader,
                                      dry_run=False)
 
-    def testStartStop(self):
+    def testWritePlausibleWeightsToDatabase(self):
+        plausibleWeight = 90
+        self.ocr.readWeight.return_value = plausibleWeight
+        self.buildMonitor()
+
         self.monitor.weightFromPictureToDatabase()
+
+        self.database.writeWeight.assert_called_once_with(plausibleWeight)
+
+    def testIgnoreImplausibleWeights(self):
+        implausibleWeight = 3
+        self.ocr.readWeight.return_value = implausibleWeight
+        self.buildMonitor()
+
+        self.monitor.weightFromPictureToDatabase()
+
+        self.database.writeWeight.assert_not_called()
