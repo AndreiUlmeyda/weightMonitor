df6d8ad9f40a3ba055c88d7d555d1de7333f63c5
Adrian Schurz <ulmeyda.smile@gmail.com>
2021-01-08 19:11:58 +0100

    Suppress warning for used pins

--- src/raspberry.py
+++ src/raspberry.py
@@ -10,6 +10,7 @@ import logging
 
 class Raspberry(RaspberryInterface):
     def __init__(self):
+        GPIO.setWarnings(False)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
