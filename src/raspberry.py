969a5f3b2b48df191e12b6a8f4ac0021400ed33c
Adrian Schurz <ulmeyda.smile@gmail.com>
2021-01-16 15:13:28 +0100

    Pylint, documentation

--- src/raspberry.py
+++ src/raspberry.py
@@ -1,20 +1,32 @@
-from src.raspberry_interface import RaspberryInterface
+# pylint: disable=E0401
+"""
+Provide a class to access Raspberry-Pi capabilities like
+detecting button presses and taking images.
+"""
 
-import RPi.GPIO as GPIO
-from picamera import PiCamera
-from PIL import Image
 import io
 from time import sleep
 import logging
+from src.raspberry_interface import RaspberryInterface
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from PIL import Image
 
 
 class Raspberry(RaspberryInterface):
+    """
+    Prepare and perform actions like taking pictures and reacting to button presses.
+    """
     def __init__(self):
+        self.pin = 10
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     def take_picture(self):
+        """
+        Take an image using PiCamera
+        """
         camera = PiCamera()
         buffer = io.BytesIO()
         camera.capture(buffer, format='jpeg', resize=(1024, 576))
@@ -23,21 +35,32 @@ class Raspberry(RaspberryInterface):
         return Image.open(buffer)
 
     def loop_and_on_button_press(self, action):
+        """
+        Wait for a button press. Rule out transient signals on the pin before executing
+        the specified action.
+        """
         # TODO remove the need for this module to know something about the action it is performing
         logging.info(
             'Please step on the scale to measure and store your weight.')
         while True:
-            if self.pinHigh():
-                if self.pinHighForAnotherWhile():
+            if self.pin_high():
+                if self.pin_high_for_another_while():
                     action()
             sleep(0.1)
 
-    def pinHighForAnotherWhile(self) -> bool:
-        pinState = self.pinHigh()
+    def pin_high_for_another_while(self) -> bool:
+        """
+        Verify that the pin state is consistently high for consecutive readings
+        in order to rule out transient signals.
+        """
+        pin_state = self.pin_high()
         for _ in range(5):
-            pinState = pinState and self.pinHigh()
+            pin_state = pin_state and self.pin_high()
             sleep(0.001)
-        return pinState
+        return pin_state
 
-    def pinHigh(self):
-        return GPIO.input(10)
+    def pin_high(self):
+        """
+        Read the pin state.
+        """
+        return GPIO.input(self.pin)
--- src/weight_monitor.py
+++ src/weight_monitor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# pylint: disable=C0103,E1101
+# pylint: disable=C0103,E1101,R0913
 """
 This module provides a class to transfer measurements from
 a bathroom scale to a database.
@@ -12,6 +12,15 @@ import logging
 
 
 class WeightMonitor:
+    """
+    Use Raspberry-Pi capabilities to detect button presses and
+    capture images to automate weight monitoring.
+    A pressure sensor is used to detect a person stepping on the scale.
+    A PiCamera is used to take an image of the scale display.
+    OCR is used to read off the value and a local Database is used to store
+    the readings.
+    Audio feedback is provided to signal start, processing, success/failure.
+    """
     def __init__(self,
                  scale_reader,
                  audio_feedback,
@@ -28,6 +37,12 @@ class WeightMonitor:
         self.delay = delay
 
     def weightFromPictureToDatabase(self) -> None:
+        """
+        Take a picture and perform OCR on the image.
+        If the result passes sanity checks (being a value of the currently possible weight range),
+        then store the result in the database.
+        If it is a dry run, then do not write anything to database.
+        """
 
         self.audio.start()
 
@@ -46,8 +61,8 @@ class WeightMonitor:
         try:
             self.weight = float(readout)
         except ValueError:
-            logging.error(
-                f"Readout '{readout}' cannot be interpreted as a number.")
+            logging.error('Readout %s cannot be interpreted as a number.',
+                          readout)
             self.audio.error()
             return
 
@@ -57,19 +72,24 @@ class WeightMonitor:
                 error = self.db.write_weight(self.weight)
 
             if error is None:
-                logging.info(
-                    f"{self.weight}kg has been commited to the database.")
+                logging.info('%skg has been commited to the database.',
+                             self.weight)
                 self.audio.success()
             else:
                 logging.error(error)
                 self.audio.error()
         else:
-            logging.error(f"Readout '{self.weight}' \
-is not in the range of assumed values between 83kg and 95kg")
+            logging.error(
+                'Readout \'%s\' is not in the range of assumed values between 83kg and 95kg',
+                self.weight)
             self.audio.error()
             return
 
     def startAndPlayProgressSound(self):
+        """
+        Starting the procedure and playing the 'in progress' sound needs
+        to happen concurrently in order to make sense.
+        """
         threadWeightToDatabase = threading.Thread(
             target=self.weightFromPictureToDatabase)
         threadSoundInProgress = threading.Thread(target=self.audio.in_progress)
@@ -79,4 +99,8 @@ is not in the range of assumed values between 83kg and 95kg")
         threadWeightToDatabase.join()
 
     def run(self):
+        """
+        Let the Raspberry-Pi interface use its capabilities to detect button presses
+        start the process.
+        """
         self.rpi.loop_and_on_button_press(self.startAndPlayProgressSound)
